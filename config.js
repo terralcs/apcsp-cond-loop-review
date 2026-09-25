d2c5dc0d9c89867ca81cab3a2dd91096c3548a5d
terralcs <tayla.terral@fayar.net>
2026-09-25 11:07:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Google Apps Script Web App URL.
 // After deploying Code.gs as a Web App, paste the URL between the quotes.
 // Example: const GOOGLE_SHEET_ENDPOINT = "https://script.google.com/macros/s/XXXXXXXX/exec";
-const GOOGLE_SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbztWf3SVdCRiOmZn_eX7ooHFyaqvnd7wJtOXzOTlDCAnd6RJ2h0hfYBmiog8k-bc46alQ/exec";
+const GOOGLE_SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbwLW0b31y2PK20G_7bV2yI2VykV3bVD4w_hdYlB-uV6xsRErcy_s9jSRylbUQDpctgw/exec";
