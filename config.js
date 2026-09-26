51d36a285cb0bc8c043741f9b5a96b7b81761beb
terralcs <tayla.terral@fayar.net>
2026-09-25 22:34:39 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Google Apps Script Web App URL.
-const GOOGLE_SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbzoH_Y90Ogn7bvDDk9QMOS4pRappz6E7-vlVevajJzY4ClHM1JOOaeQrb_q0l8qfu20Ww/exec";
+const GOOGLE_SHEET_ENDPOINT = "https://script.google.com/macros/s/AKfycbxD_rUfYk8Ve7WZv3VanTfQHadU5Fj2WhQj1snpw-O4bFLDdEW7lzXcOHkpezfUiuBZLA/exec";
